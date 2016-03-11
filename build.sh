83fa45b4eadef4e1901dcf9c11fcf2a2d3c8e9db
Tyler Cromwell <tyler@csh.rit.edu>
2016-03-11 18:06:59 -0500

    Adding my OPENCV build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OPENCV=~/opencv
+OPENCV_CONTRIB_MODULES=~/opencv_contrib/modules
+
+if [[ -z $OPENCV || -z $OPENCV_CONTRIB_MODULES ]]; then
+    echo "Repository path missing!"
+    echo $OPENCV
+    exit 1
+fi
+
+mkdir -p $OPENCV/build
+cd $OPENCV/build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D WITH_OPENGL=ON \
+      -D WITH_TBB=ON -D BUILD_TBB=ON \
+      -D INSTALL_C_EXAMPLES=OFF \
+      -D INSTALL_PYTHON_EXAMPLES=OFF \
+      -D OPENCV_EXTRA_MODULES_PATH=$OPENCV_CONTRIB_MODULES \
+      -D BUILD_EXAMPLES=OFF $OPENCV
