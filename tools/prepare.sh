664e56a99d61206273c8dda09f6ec62a1e581ff8
Tyler Cromwell <tjc6185@gmail.com>
2017-09-16 00:53:13 -0400

    Changed email in prepare.sh and add header to install-opencv-deps-ubuntu.sh

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ########################################################################
-#  Copyright (C) 2016 Tyler Cromwell <tyler@csh.rit.edu>
+#  Copyright (C) 2016 Tyler Cromwell <tjc6185@gmail.com>
 #
 #  This file is part of Cerebrum.
 #
